13c729d011bde58d8090d72087b7e670c393ac47
pmp-p <mail.peny@free.fr>
2025-04-14 07:58:30 +0200

    3.1.61.7pre

--- get/python.sh
+++ get/python.sh
@@ -5,7 +5,7 @@ echo "
     * extracting Python 3.13.x to $(pwd)
 "
 
-wget -q https://www.python.org/ftp/python/3.13.2/Python-3.13.2.tar.xz -O/tmp/python.tar.xz
+wget -q https://www.python.org/ftp/python/3.13.3/Python-3.13.3.tar.xz -O/tmp/python.tar.xz
 tar xfp /tmp/python.tar.xz
 rm /tmp/python.tar.xz
 
