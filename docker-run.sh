ef51882069d6143dfac05a561c90e2937c7373dd
pmp-p <mail.peny@free.fr>
2025-07-10 08:23:53 +0200

    test ci

---
+++ docker-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# or use -rm on docker run cmdline
+docker container rm wasmsdk
+
+if echo -n $@|grep -q it$
+then
+    PROMPT="&& bash ) || bash"
+else
+    PROMPT=")"
+fi
+
+
+docker run $@ \
+ -e SDKROOT=$SDKROOT \
+ -e EMFLAVOUR=$EMFLAVOUR \
+ -e BUILDS=3.13 \
+ -e wasisdk=true \
+ -v ./python-wasm-sdk:/workspace \
+ -v ./prebuilt:/workspace/prebuilt \
+ -v ${CONTAINER_PATH}${SDKROOT}/dist:${SDKROOT}/dist \
+ --workdir=/workspace --name wasmsdk debian:12 \
+ bash --noprofile --rcfile docker_rc.sh -ci "( ./python-wasm-sdk.sh $PROMPT"
+
